9a7692b15bfbf6abb8a9bc964dafcf3ece597391
Sunil Sharma <ssharmaca494@gmail.com>
2018-11-04 18:45:31 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div id="example1"></div>
+
+        <script src="{{ URL::asset('/js/app.js') }}"></script>  
+    </body>
+</html>
